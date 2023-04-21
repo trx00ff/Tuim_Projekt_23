838706c975fd4ba54d7a982b3f88ccde8efa5061
Krlyn <karolina.janiak@student.wat.edu.pl>
2023-04-21 13:57:05 +0200

    Initial commit

---
+++ tuim23/src/main/java/com/krlyn/tuim23/Tuim23Application.java
@@ -0,0 +1,13 @@
+package com.krlyn.tuim23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuim23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuim23Application.class, args);
+	}
+
+}
---
+++ tuim23/src/test/java/com/krlyn/tuim23/Tuim23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krlyn.tuim23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuim23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
