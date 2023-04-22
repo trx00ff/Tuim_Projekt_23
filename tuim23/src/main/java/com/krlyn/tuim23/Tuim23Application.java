5495307e822684e87f85819c611f99fe951a9750
Krlyn <karolina.janiak@student.wat.edu.pl>
2023-04-22 15:24:43 +0200

    Update nr 1

--- tuim23/src/main/java/com/krlyn/tuim23/Tuim23Application.java
+++ tuim23/src/main/java/com/krlyn/tuim23/Tuim23Application.java
@@ -2,8 +2,9 @@ package com.krlyn.tuim23;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan({"com.krlyn.tuim23.controller"})
 public class Tuim23Application {
 
 	public static void main(String[] args) {
